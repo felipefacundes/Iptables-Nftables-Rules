c64e4bab8f550bb7cf80ef69a9571497e7dd0b96
Felipe Facundes <felipe.facundes@gmail.com>
2020-10-18 03:18:23 -0300

    Update README.sh

--- README.sh
+++ README.sh
@@ -164,15 +164,15 @@
 #
 
 # Best Scrip all block
-echo -e $(seq -f "iptables -A INPUT -p tcp --dport %g -j DROP;" 65535) > /tmp/tcp-block-all-ports.sh
-echo -e $(seq -f "iptables -A OUTPUT -p tcp --dport %g -j DROP;" 65535) >> /tmp/tcp-block-all-ports.sh
-echo -e $(seq -f "iptables -A INPUT -p udp --dport %g -j DROP;" 65535) > /tmp/udp-block-all-ports.sh
-echo -e $(seq -f "iptables -A OUTPUT -p udp --dport %g -j DROP;" 65535) >> /tmp/udp-block-all-ports.sh
+echo -e $(seq -f "iptables -A INPUT -p tcp --dport %g -j DROP\n" 65535) > /tmp/tcp-block-all-ports.sh
+echo -e $(seq -f "iptables -A OUTPUT -p tcp --dport %g -j DROP\n" 65535) >> /tmp/tcp-block-all-ports.sh
+echo -e $(seq -f "iptables -A INPUT -p udp --dport %g -j DROP\n" 65535) > /tmp/udp-block-all-ports.sh
+echo -e $(seq -f "iptables -A OUTPUT -p udp --dport %g -j DROP\n" 65535) >> /tmp/udp-block-all-ports.sh
 #
-echo -e $(seq -f "iptables -A INPUT -p sctp --dport %g -j DROP;" 65535) > /tmp/sctp-block-all-ports.sh
-echo -e $(seq -f "iptables -A OUTPUT -p sctp --dport %g -j DROP;" 65535) >> /tmp/sctp-block-all-ports.sh
-echo -e $(seq -f "iptables -A INPUT -p dccp --dport %g -j DROP;" 65535) > /tmp/dccp-block-all-ports.sh
-echo -e $(seq -f "iptables -A OUTPUT -p dccp --dport %g -j DROP;" 65535) >> /tmp/dccp-block-all-ports.sh
+echo -e $(seq -f "iptables -A INPUT -p sctp --dport %g -j DROP\n" 65535) > /tmp/sctp-block-all-ports.sh
+echo -e $(seq -f "iptables -A OUTPUT -p sctp --dport %g -j DROP\n" 65535) >> /tmp/sctp-block-all-ports.sh
+echo -e $(seq -f "iptables -A INPUT -p dccp --dport %g -j DROP\n" 65535) > /tmp/dccp-block-all-ports.sh
+echo -e $(seq -f "iptables -A OUTPUT -p dccp --dport %g -j DROP\n" 65535) >> /tmp/dccp-block-all-ports.sh
 
 sudo chmod +x /tmp/*-block-all-ports.sh
 sudo /tmp/*-block-all-ports.sh
