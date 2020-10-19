04c0bb608b16e53f30aa83b4b418675bcbc43c73
Felipe Facundes <felipe.facundes@gmail.com>
2020-10-19 13:31:10 -0300

    Update ipt2nft.sh

--- config-rules/scripts/ipt2nft.sh
+++ config-rules/scripts/ipt2nft.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-iptables-save > /tmp/iptables-ruleset.txt
-iptables-restore-translate -f /tmp/iptables-ruleset.txt
-iptables-restore-translate -f /tmp/iptables-ruleset.txt > /tmp/ruleset.nft
+iptables-save > /tmp/iptables.rules
+iptables-restore-translate -f /tmp/iptables.rules
+iptables-restore-translate -f /tmp/iptables.rules > /tmp/ruleset.nft
 nft -f /tmp/ruleset.nft
 su -c "nft list ruleset > /etc/nftables.conf"
+systemctl start nftables.service
+systemctl enable nftables.service
 nft list ruleset
