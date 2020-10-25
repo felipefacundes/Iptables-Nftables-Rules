517a68b99b8efc3e67d0003bcfbd36dfb48e67a1
Felipe Facundes <felipe.facundes@gmail.com>
2020-10-25 16:06:44 -0300

    Update ip6tables.rules.sh

--- config-rules/scripts/ip6tables.rules.sh
+++ config-rules/scripts/ip6tables.rules.sh
@@ -9,7 +9,7 @@ ipt6="/sbin/ip6tables"
 # Flush all rules and delete all chains
 # for a clean startup
 $ipt6 -F
-$ipt6 -X 
+$ipt6 -X
 
 # Zero out all counters
 $ipt6 -Z
@@ -33,20 +33,18 @@ $ipt6 -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 # Accept all ICMP v6 packets
 $ipt6 -A INPUT -p ipv6-icmp -j ACCEPT
 
-# Optional rules to allow other LAN hosts access 
+# Optional rules to allow other LAN hosts access
 # to services. Delete $ipt6 -A INPUT -p tcp --syn -j DROP
 
 # Allow DHCPv6 from LAN only
-$ipt6 -A INPUT -m state --state NEW -m udp -p udp 
--s fe80::/10 --dport 546 -j ACCEPT
+$ipt6 -A INPUT -m state --state NEW -m udp -p udp -s fe80::/10 --dport 546 -j ACCEPT
 
 # Allow connections from SSH clients
 $ipt6 -A INPUT -m state --state NEW -m tcp -p tcp --dport 22 -j ACCEPT
 
-# Allow HTTP and HTTPS traffic 
+# Allow HTTP and HTTPS traffic
 $ipt6 -A INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
 $ipt6 -A INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
 
 # Allow access to SMTP, POP3, and IMAP
-$ipt -A INPUT -m state --state NEW -p tcp -m multiport 
---dport 25,110,143 -j ACCEPT
+$ipt -A INPUT -m state --state NEW -p tcp -m multiport --dport 25,110,143 -j ACCEPT
